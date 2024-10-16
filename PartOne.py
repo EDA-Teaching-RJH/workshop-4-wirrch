13841cdec8caabe0fd7d616a349986b3fe49a4f2
wirrch <wgb4@kent.ac.uk>
2024-10-16 12:18:56 +0100

    creation

--- PartOne.py
+++ PartOne.py
@@ -1,1 +1,11 @@
+userInput = input("Name of variable in camelCase: ")
+newString = ""
+
+for c in userInput:
+    if c == c.upper():
+        newString += f"_{c.lower()}"
+    else:
+        newString += c
+
+print(newString)
 
--- PartTwo.py
+++ PartTwo.py
@@ -1,1 +1,29 @@
+def main():
+    userInput = input("What drink would you like? : ").lower()
+    match userInput:
+        case "hot chocolate":
+            print(f"Required moneyz = 120")
+            transaction(120)
+        case "tea":
+            print(f"Required moneyz = 100")
+            transaction(100)
+        case _:
+            print("Invalid drink")
+            main()
 
+
+
+def transaction(reqMoneyz):
+    totalMoney = 0
+
+    while totalMoney < reqMoneyz:
+        userInput = int(input("Insert a pence coin: "))
+        match userInput:
+            case 50 | 20 | 10 | 5:
+                totalMoney += userInput
+        print(f"Total moneyz inserted: {totalMoney}")
+
+    print(f"Change due: {totalMoney - 75}")
+
+
+main()
